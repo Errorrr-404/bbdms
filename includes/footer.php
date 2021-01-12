19da021ba357e583a6cbc5bcb3822751e56931e7
Sarvesh <sarvesh.chp@gmail.com>
2021-01-12 20:41:33 +0530

    1st Commit

---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+  
+  <div style="margin-top:300px"></div>
+  <footer class="py-2 bg-inverse">
+        <div class="container">
+            <p class="m-0 text-center text-white">Copyright &copy; BloodBank & Donor Management System 2020</p>
+        </div>
+        <!-- /.container -->
+    </footer>
